e8ae5fa595bd198de5eb694d7fd93dba2c0b717a
BakerC1216 <31607841+BakerC1216@users.noreply.github.com>
2017-09-10 08:33:21 -0400

    Add files via upload
    First commit

---
+++ M2T1_SalesPrediction_Baker.py
@@ -0,0 +1,11 @@
+# CTI-110
+# M2T1 - Sales Prediction
+# BakerC1216
+# 9/10/17
+total_sales = float (input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
